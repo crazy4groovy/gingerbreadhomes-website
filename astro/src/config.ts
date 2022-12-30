a8d90022b9840d519b189fcd0177e55f565d16be
solsen <spam2steve@gmail.com>
2022-12-29 22:13:55 -0700

    astro framework conversion

---
+++ astro/src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Gingerbread Houses';
+export const SITE_DESCRIPTION = 'Housing that is fun AND functional';
